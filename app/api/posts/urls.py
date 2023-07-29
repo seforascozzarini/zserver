94641e0448e59696d1b15defe133a0506924a012
Sefora Scozzarini <sefora.scozzarini@gmail.com>
2023-07-29 16:05:42 +0200

    Added Post fields for search

--- app/api/posts/urls.py
+++ app/api/posts/urls.py
@@ -11,5 +11,4 @@ app_name = 'posts'
 urlpatterns = [
     path('create/', views.CreatePostView.as_view(), name='create'),
     path('get/', views.GetPostListView.as_view(), name='get_list'),
-    path('get_posts_by/', views.GetPostByText.as_view(), name='get_post_by'),
 ]
--- app/api/posts/views.py
+++ app/api/posts/views.py
@@ -9,6 +9,8 @@ from rest_framework import (
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
 
 from api.posts.serializers import (
     PostCreateSerializer,
@@ -32,58 +34,76 @@ class GetPostListView(generics.ListAPIView):
 
     def get_queryset(self):
         id = self.request.query_params.get('id')
-        user_id = self.request.query_params.get('user_id')
-        post_code = self.request.query_params.get('code')
         post_type = self.request.query_params.get('post_type')
         status = self.request.query_params.get('status')
+        user_id = self.request.query_params.get('user_id')
+        post_code = self.request.query_params.get('code')
+        pet_type = self.request.query_params.get('pet_type')
+        gender = self.request.query_params.get('gender')
+        microchip = self.request.query_params.get('microchip')
+        sterilised = self.request.query_params.get('sterilised')
+        location = self.request.query_params.get('location')
+        address = self.request.query_params.get('address')
+        age = self.request.query_params.get('age')
+        specific_marks = self.request.query_params.get('specific_marks')
+        pet_name = self.request.query_params.get('pet_name')
+        text = self.request.query_params.get('text')
+        contacts = self.request.query_params.get('contacts')
+        event_date = self.request.query_params.get('event_date')
 
+        post_list = Post.objects.all()
         post_list = post_list.filter(status=status) if status else Post.objects.all()
 
+        if id:
+            post_list = post_list.filter(id=id)
+        if post_type:
+            post_list = post_list.filter(type=post_type)
         if user_id:
             post_list = post_list.filter(user_id=user_id)
         if post_code:
             post_list = post_list.filter(code=post_code)
-        if post_type:
-            post_list = post_list.filter(type=post_type)
-        if id:
-            post_list = post_list.filter(id=id)
+        if pet_type:
+            post_list = post_list.filter(pet_type=pet_type)
+        if gender:
+            post_list = post_list.filter(gender=gender)
+        if microchip:
+            post_list = post_list.filter(microchip=microchip)
+        if sterilised:
+            post_list = post_list.filter(sterilised=sterilised)
+        if location:
+            post_list = post_list.filter(location=location)
+        if address:
+            post_list = post_list.filter(address=address)
+        if age:
+            post_list = post_list.filter(age=age)
+        if specific_marks:
+            post_list = post_list.filter(specific_marks=specific_marks)
+        if pet_name:
+            post_list = post_list.filter(pet_name=pet_name)
+        if text:
+            post_list = post_list.filter(text=text)
+        if contacts:
+            post_list = post_list.filter(contacts=contacts)
+        if event_date:
+            post_list = post_list.filter(event_date=event_date)
 
         return post_list
 
 
-class GetPostByText(generics.ListAPIView):
-    """"Get a post list filtered by text in the system."""
+# TODO AGGIUNGERE RICERCA DELLA DISTANZA DA UN PUNTO
+class GetPostLocation(generics.ListAPIView):
+    """Get a post list filtered by location in the system."""
     http_method_names = ['get']
     serializer_class = PostListSerializer
 
     def get_queryset(self):
-        text_to_search = self.request.query_params.get('text')
-        post_list = None
-        if Post.objects.filter(type=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(location=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(pet_type=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(gender=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(age=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(microchip=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(sterilised=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(specifics_marks=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(pet_name=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(text=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(contacts=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(status=text_to_search):
-            post_list.append(Post)
-        if Post.objects.filter(event_date=text_to_search):
-            post_list.append(Post)
+        location = self.request.query_params.get('location')
+        longitude = self.request.query_params.get('longitude')
+        latitude = self.request.query_params.get('latitude')
+        radius = self.request.query_params.get('radius')
 
-        return post_list
+        location = Point(longitude, latitude)
+
+        queryset = Post.objects.filter(location__distance_lte=(location, D(m=location.distance))).distance(location).order_by('distance')
+
+        return queryset
